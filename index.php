ba9ce7381b6645bc0cd3ca55fd388c8ee3b2246d
angelwidjaja <31278015+angelwidjaja@users.noreply.github.com>
2019-06-25 10:19:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
             $email = $_POST['email'];
             // Insert data
             $sql_insert = "INSERT INTO Users (name, phone, email) 
-                        VALUES (?,?,?,?)";
+                        VALUES (?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
             $stmt->bindValue(2, $phone);
