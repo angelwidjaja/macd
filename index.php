3d9a69ab644594b8ec424995dae481cf366bbeac
angelwidjaja <31278015+angelwidjaja@users.noreply.github.com>
2019-06-25 10:11:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
             $email = $_POST['email'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO dbo.Users (name, phone, email, date) 
+            $sql_insert = "INSERT INTO Users (name, phone, email, date) 
                         VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
@@ -57,7 +57,7 @@
         echo "<h3>Your're registered!</h3>";
     } else if (isset($_POST['load_data'])) {
         try {
-            $sql_select = "SELECT * FROM dbo.Users";
+            $sql_select = "SELECT * FROM Users";
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
